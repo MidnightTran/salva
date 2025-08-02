1a91c0939c9870dcd5a9adcdf8e671f5a4a20adf
MidnightTran <MidnightTranTV@gmail.com>
2025-08-02 19:02:41 -0400

    Testing collisions

--- src/integrations/rapier/fluids_pipeline.rs
+++ src/integrations/rapier/fluids_pipeline.rs
@@ -205,6 +205,9 @@ impl<'a> CouplingManager for ColliderCouplingManager<'a> {
                             match particle {
                                 HGridEntry::FluidParticle(fluid_id, particle_id) => {
                                     let fluid = &mut fluids[*fluid_id];
+                                    if !boundary.interaction_groups.test(fluids[*fluid_id].interaction_groups) {
+                                        continue;
+                                    }
                                     let particle_pos = fluid.positions[*particle_id]
                                         + fluid.velocities[*particle_id] * timestep.dt();
 
